eabf19c6803b6aefd6088b44904439beb216fe72
ssridhar128 <79930825+ssridhar128@users.noreply.github.com>
2025-04-06 04:04:45 -0700

    Comments trying to use our data to populate(not working)

--- flask-template/app.py
+++ flask-template/app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
+# @app.route('/search', methods = ['GET'])
+# def search_products():
+#     search_term = request.agrs.get('query', '')
+#     if not search_term:
+#         return jsonify({"error":"No search term provided"}), 400
+#     product_data = search(search_term)
+#     return jsonify(product_data)
 
 @app.route("/")
 def home():
@@ -11,3 +18,6 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+# if __name__ == '__main__':
+#     app.run(debug = True)
